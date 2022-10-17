61198ac00707e38090b9d236cb9b5aa272ce31fe
hadwapasha <hadwap@gmail.com>
2022-10-18 00:09:06 +0200

    my solution

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ const express = require("express");
 const mongoose = require('mongoose');
 // THIS IS WRONG NEVER DO THAT !! Only for the task we put the DB Link here!! NEVER DO THAAAT AGAIN !!
 //Check db connection links in README file
-const MongoURI = '' ;
+const MongoURI = 'mongodb+srv://Hadwa:hadwa1996@cluster0.hpstsct.mongodb.net/?retryWrites=true&w=majority' ;
+
+const {createUser, getUsers}= require('./Routes/userController')
 
 
 //App variables
 const app = express();
 const port = process.env.PORT || "8000";
-const user = require('./models/User');
+const user = require('./Models/User');
 // #Importing the userController
 
 
@@ -31,12 +33,13 @@ app.get("/home", (req, res) => {
     res.status(200).send("You have everything installed!");
   });
 
+app.use(express.json())
+app.post("/addUser",createUser);
+app.get('/users', getUsers);
 // #Routing to userController here
-
 
 
 
 /*
                                                     End of your code
 */
-
--- src/Routes/userController.js
+++ src/Routes/userController.js
@@ -1,1 +1,21 @@
 // #Task route solution
+const userModel = require('../Models/User');
+// const mongoose = require('mongoose');
+
+const createUser = async(req,res) => {
+    const{Name, Email, Age, BornIn, LivesIn, MartialStatus, PhoneNumber, Job} = req.body;
+    try{
+        const user = await userModel.create({Name, Email, Age, BornIn, LivesIn, MartialStatus, PhoneNumber, Job});
+        res.status(200).json(user)
+    }catch(error){
+        res.status(400).json({error:error.message})
+    }
+}
+
+const getUsers = async (req, res) => {
+    const users = await userModel.find({}).sort({createdAt: -1})
+  
+    res.status(200).json(users)
+  }
+
+module.exports = {createUser, getUsers};
